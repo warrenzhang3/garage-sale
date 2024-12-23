e82f8e779b4edd11b1f4d0836727ee6b2b0ec1fd
Warren Zhang <warrenzhangls@gmail.com>
2024-12-23 17:43:24 -0500

    added items data into seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,44 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+## users
+
 puts 'Creating users...'
 user1 = User.new(
   first_name: "Warren",
   last_name: "Zhang",
   email: "warren@email.com",
-  password: "123456")
+  password: "123456"
+)
 user1.save!
 
 user2 = User.new(
   first_name: "User",
   last_name: "Two",
   email: "user2@email.com",
-  password: "123456")
+  password: "123456"
+)
 user2.save!
+puts 'Finished!'
+
+## items
+
+puts 'Creating items...'
+item1 = Item.new(
+  title: "Recycled Paper",
+  description: "A4 white coloured recycled paper.",
+  price: 70.00,
+  size: 100,
+  user: user1
+)
+item1.save!
+
+item2 = Item.new(
+  title: "Rocking Chair",
+  description: "Old armchair rocking chair.",
+  price: 320.00,
+  size: 2,
+  user: user1
+)
+item2.save!
 puts 'Finished!'
